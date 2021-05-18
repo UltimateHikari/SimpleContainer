fdea8df025a89a38ff3f84b0c32e2ab5cce0f3fb
UltimateHikari <andy@andystpad.localdomain>
2021-05-18 21:30:54 +0700

    still broken manual

--- manual/scripts/cont_nssetup.sh
+++ manual/scripts/cont_nssetup.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-# $1 is host number, up to 8 total
-# $2 is container number, 2 - 8
+# $1 is container number, 2 - 8
 
-ip netns add cont$2
-ip link add dev veth$20 type veth peer name veth$21
-ip link set dev veth$20 netns overlay
-ip -n overlay link set dev veth$20 master br0
-ip -n overlay link set veth$20 up
+ip netns add cont$1
+ip link add dev veth$10 type veth peer name veth$11
+ip link set dev veth$10 netns overlay
+ip -n overlay link set dev veth$10 master br0
+ip -n overlay link set veth$10 up
 
-ip link set dev veth$21 netns cont$2
-ip -n cont$2 link set dev veth$21 name eth0 address 02:42:c0:a8:0$1:0$2
-ip -n cont$2 addr add dev eth0 172.172.0.$2/24
-ip -n cont$2 link set dev eth0 up
+ip link set dev veth$11 netns cont$1
+ip -n cont$1 link set dev veth$11 name eth0 address 02:42:c0:a8:00:0$1
+ip -n cont$1 addr add dev eth0 10.0.0.$1/24
+ip -n cont$1 link set dev eth0 up
--- manual/scripts/host_route_setup.sh
+++ manual/scripts/host_route_setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# $1 - current container num
-# $2 - ipv4 of other node
-# $3 - host number
-ip -n overlay ip neighbor add 172.172.0.$1 lladdr 02:42:c0:a8:0$3:0$1 dev vxlan1
-ip -n overlay bridge fdb add 02:42:c0:a8:0$3:0$1 dev vxlan1 self dst $2 vni 42 port 4789
+# $1 - other container num
+# $2 - other node num
+ip -n overlay neighbor add 10.10.0.$1 lladdr 02:42:c0:a8:00:0$1 dev vxlan1
+ip netns exec overlay bridge fdb add 02:42:c0:a8:00:0$1 dev vxlan1 self dst 10.0.0.$2 vni 42 port 4789
